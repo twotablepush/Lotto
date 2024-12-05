8958c27fdfa662d0ecfe1e991000a803953bb627
twotablepush <ssangmin7252@gmail.com>
2024-12-05 15:45:43 +0900

    add: one-hot Encoding

--- backend/scripts/preprocess.py
+++ backend/scripts/preprocess.py
@@ -4,6 +4,23 @@ import numpy as np
 # 데이터 로드
 data = pd.read_excel("/Users/lsm/Desktop/study/Lotto/backend/data/raw/Lotto_data.xlsx")
 
-# 데이터 확인
-print("원본 데이터:")
-print(data.head())  # 상위 5개 행 출력
+# 번호 범위 (1 ~ 45)
+num_range = np.arange(1, 46)
+
+# 각 회차의 번호를 원-핫 인코딩
+one_hot_encoded = data.iloc[:, 1:7].apply(
+    lambda row: np.isin(num_range, row).astype(int), axis = 1
+)
+
+# 원-핫 결과를 데이터프레임으로 변환
+one_hot_df = pd.DataFrame(
+    np.vstack(one_hot_encoded),
+    columns=[f"번호_{i}" for i in range(1, 46)]
+)
+
+# 기존 데이터와 결합
+final_data = pd.concat([data, one_hot_df], axis = 1)
+
+# 결합된 데이터 확인
+print("\n최종 데이터:")
+print(final_data.head())
